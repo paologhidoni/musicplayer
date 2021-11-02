4533c33549dec1cf1aca91c60e36f014f210df04
Paolo <paolo.ghidoni82@gmail.com>
2021-11-02 15:49:44 +0000

    Display total length of track in timer, tidy up JS code

--- app.js
+++ app.js
@@ -70,15 +70,17 @@ const numSongsInPlaylist = Object.keys(playlist).length;
 
 // Function to load song and update song details
 function loadSong(playlist, currentSong) {
-  artist.innerText = `${playlist[currentSong].artist}`;
+  artist.textContent = `${playlist[currentSong].artist}`;
   title.textContent = `${playlist[currentSong].songName}`;
   audioElement.src = `assets/audio/${playlist[currentSong].source}`;
   cover.src = `assets/img/${playlist[currentSong].cover}`;
   songURL.href = `${playlist[currentSong].songURL}`;
 }
 
+
 // Run Load song
 loadSong(playlist, currentSong);
+
 
 // Function to play song
 function playSong() {
@@ -88,6 +90,7 @@ function playSong() {
   audioElement.play();
 }
 
+
 // Function to pause the song
 function pauseSong() {
   player.classList.remove('play');
@@ -95,6 +98,7 @@ function pauseSong() {
 
   audioElement.pause();
 }
+
 
 // Function to skip to the previous song
 function prevSong() {
@@ -109,6 +113,7 @@ function prevSong() {
   playSong();
 }
 
+
 // Function to skip to the next song
 function nextSong() {
   if(currentSong < Object.keys(playlist).length - 1) {
@@ -120,6 +125,7 @@ function nextSong() {
   loadSong(playlist, currentSong);
   playSong();
 }
+
 
 // Function to skip to random song - shuffle functionality
 function skipToRandom() {
@@ -136,6 +142,7 @@ function skipToRandom() {
 
 }
 
+
 // Function to display countdown timer and update the progress bar.
 function updateProgress(e) {
   const {duration, currentTime} = e.srcElement; // Store in variables total length of 
@@ -144,22 +151,30 @@ function updateProgress(e) {
   playerBarForeground.style.width = `${progressPercent}%`; // Apply percentage as width style to progress bar.
 
 
-  const minutes = Math.floor(audioElement.currentTime / 60); // Find elapsed minutes.
-  const displayMinutes = minutes < 10 ? '0' + minutes : minutes; // Pad to 2 digits.
+  // Calculate and format total length of track
+  const totMinutes = Math.floor(duration / 60); // Find minutes
+  const displayTotMinutes = totMinutes < 10 ? '0' + totMinutes : totMinutes; // Pad to 2 digits
+  const totSeconds = Math.floor(duration - totMinutes * 60); // Find seconds 
+  const displayTotSeconds = totSeconds < 10 ? '0' + totSeconds : totSeconds; // Pad to 2 digits
 
-  const seconds = Math.floor(audioElement.currentTime - minutes * 60); // Find elapsed seconds.
-  const displaySeconds = seconds < 10 ? '0' + seconds : seconds; // Pad to 2 digits.
-  timer.innerText = `${displayMinutes}:${displaySeconds}`; // display minutes and seconds.
+  // Calculate and format Elapsed time
+  const elapsedMinutes = Math.floor(currentTime / 60); // Find elapsed minutes.
+  const displayElapsedMinutes = elapsedMinutes < 10 ? '0' + elapsedMinutes : elapsedMinutes; // Pad to 2 digits.
+  const elapsedSeconds = Math.floor(currentTime - elapsedMinutes * 60); // Find elapsed seconds.
+  const displayElapsedSeconds = elapsedSeconds < 10 ? '0' + elapsedSeconds : elapsedSeconds; // Pad to 2 digits.
+
+  // Display elapsed time and total length in timer
+  timer.textContent = ` ${displayElapsedMinutes}:${displayElapsedSeconds} / ${displayTotMinutes}:${displayTotSeconds}`; // Display elapsed minutes & seconds and total length of track.
 }
 
-// function to make the progress bar clickable.
 
+// function to make the progress bar clickable.
 function setProgress(e) {
-  const width = this.clientWidth; // Finds total width of progress bar
-  const clickX = e.offsetX; // Finds width at clicked point
-  const duration = audioElement.duration; // get the song duration
+  const width = this.clientWidth; // Find total width of progress bar
+  const clickX = e.offsetX; // Find width at clicked point
+  const duration = audioElement.duration; // Get the song duration
   
-  audioElement.currentTime = (clickX / width) * duration; // set the audio current time to equal the width at the point where I have clicked / the total width * the song duraton.
+  audioElement.currentTime = (clickX / width) * duration; // Set the audio current time to equal the width at the point where I have clicked / the total width * the song duraton.
 }
 
 
@@ -184,20 +199,26 @@ playBtn.addEventListener('click', function () {
   }
 })
 
+
 // Add event listener to previous button to skip to previous song
 prevBtn.addEventListener('click', prevSong);
+
 
 // Add event listener to next button to skip to next song
 nextBtn.addEventListener('click', nextSong);
 
+
 // Add event listener to the shuffle button to skip to a random song
 shuffleBtn.addEventListener('click', skipToRandom);
+
 
 // Add event listener to the audio element to update progress bar
 audioElement.addEventListener('timeupdate', updateProgress);
 
+
 // Add event listener to make progress bar clickable
 playerBarBackground.addEventListener('click', setProgress);
+
 
 // Add event listener to audio element to skip to next song when previous is ended, by calling nextSong()
 audioElement.addEventListener('ended', nextSong);
